84a9ba0f1b945f0fc887c86e750f3d45f1260200
yahanccc <chelciaov@gmail.com>
2025-05-20 15:52:41 +0800

    resolve wrapper.scss conflict

---
+++ src/components/itemList.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="itemList">
+    <item 
+      v-for="(item, index) in items" 
+      :key="index" 
+      :item="item"
+      @add-to-cart="onAddToCart"
+    />
+  </ul>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+import item from './item.vue';
+
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true
+  }
+});
+
+const emit = defineEmits(['add-to-cart']);
+
+const onAddToCart = (item) => {
+  emit('add-to-cart', item);
+};
+</script>
