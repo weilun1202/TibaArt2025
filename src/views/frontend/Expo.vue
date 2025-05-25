d2d655d0bb872e703bd5de509bcd9a9b5f029e3d
HONG-1005 <A0932120249@GMAIL.COM>
2025-05-25 23:19:56 +0800

    新增 展區scss跟更改線上展區grid排法

--- src/views/frontend/Expo.vue
+++ src/views/frontend/Expo.vue
@@ -1,63 +1,28 @@
 <template>
-  <div>
-    <div class="wrapper">
-      <header>
+<div class="wrapper">
+        <header>
         <div class="pageTitle">線上展覽</div>
       </header>
-      <!-- 內容全寬，若要限制寬度再另外包 div -->
-      <div class="masonry" id="masonry">
-        <div
-          v-for="item in items"
-          :key="item.id"
-          class="item"
-          :style="{ height: item.height + 'px' }"
-        >
-          Item {{ item.id }}
-        </div>
-      </div>
-    </div>
-  </div>
+<div class="parent">
+<router-link to="/ExpoArea" class="div1">圖片</router-link>
+<div class="div2">圖片</div>
+<div class="div3">圖片</div>
+<div class="div4">圖片</div>
+<div class="div5">圖片</div>
+<div class="div6">圖片</div>
+<div class="div7">圖片</div>
+<div class="div8">圖片</div>
+<div class="div9">圖片</div>
+<div class="div10">圖片</div>
+<div class="div11">圖片</div>
+<div class="div12">圖片</div>
+<div class="div13">圖片</div>
+<div class="div14">圖片</div>
+<div class="div15">圖片</div>
+<div class="div16">圖片</div>
+<div class="div17">圖片</div>
+<div class="div18">圖片</div>
+<div class="div19">圖片</div>
+</div>
+</div>
 </template>
-
-<script setup>
-import { ref, onMounted } from 'vue';
-
-const minH = 100;
-const maxH = 250;
-const count = 17; // 項目個數，可依需求調整
-const items = ref([]);
-
-onMounted(() => {
-  items.value = Array.from({ length: count }, (_, idx) => {
-    const height = Math.floor(Math.random() * (maxH - minH + 1)) + minH;
-    return {
-      id: idx + 1,
-      height,
-    };
-  });
-});
-</script>
-
-<style scoped>
-/* 多欄式瀑布流佈局 */
-.masonry {
-  column-count: 4;
-  column-gap: 1rem;
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 1rem 0;
-}
-
-.masonry .item {
-  display: inline-block;
-  width: 100%;
-  margin-bottom: 1rem;
-  background: #87ceeb;
-  border-radius: 4px;
-  box-sizing: border-box;
-  padding: 0.5rem;
-  color: #fff;
-  font-size: 14px;
-  text-align: center;
-}
-</style>
