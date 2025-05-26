eb752ac5530d3cc341e7f9bb7b6e5d5552736dbf
HONG-1005 <A0932120249@GMAIL.COM>
2025-05-27 00:25:22 +0800

    關於我們 CSS調整，藝術家圖片上傳

--- src/views/frontend/ExpoArea.vue
+++ src/views/frontend/ExpoArea.vue
@@ -6,6 +6,22 @@
       </header>
       <!-- Wrapper 不設 1200px 寬（例如展覽、關於我們頁面應該會滿版），想要限制內容在 1200 的再自己包一個 div 限制 1200  -->
       <!-- 以下供大家編輯 -->
+      <div class="banner">
+        <div class="banner-text">
+          <h1>靜界焰光</h1>
+          <p>The Luminous Silence</p>
+          <p>「當情感無以為繼，色彩替我們說話。」 凱婭·露希亞的創作長年在寂靜與爆發之間來回拉扯，她以自然隱喻人心，讓畫布成為風暴中心的靜止點。
+            本次展覽精選其近十年間五件代表作，以色彩為線索，從冷冽冰縫、綠林密靜，到炙熱的內燃記憶，呈現一場無語卻極具張力的感官旅程。觀者不需理解，僅需感受</p>
+        </div>
+        <img src="" alt="">
+      </div>
+
+      <div class="expoArea-intro">
+        
+      </div>
+
+
+
       <div class="card">
         <img src="" alt="">
         <div class="title">
@@ -30,57 +46,13 @@
         <a href="">leam more</a>
       </div>
 
-      <div class="card">
-        <img src="" alt="">
-        <div class="title">
-          <h3>裂縫之下</h3>
-          <p>Beneath the Fracture</p>
-          <p>油畫</p>
-        </div>
-        <p>本畫靈感來自她親眼目睹的冰川裂縫，畫面以大面積裂痕與流動線條組成，象徵人內在的情緒斷層。她用此畫探討壓抑情感之下的潛流，並相信裂縫不是崩壞，而是讓光進入的通道。</p>
-        <a href="">leam more</a>
-      </div>
-
-      <div class="card">
-        <img src="" alt="">
-        <div class="title">
-          <h3>裂縫之下</h3>
-          <p>Beneath the Fracture</p>
-          <p>油畫</p>
-        </div>
-        <p>本畫靈感來自她親眼目睹的冰川裂縫，畫面以大面積裂痕與流動線條組成，象徵人內在的情緒斷層。她用此畫探討壓抑情感之下的潛流，並相信裂縫不是崩壞，而是讓光進入的通道。</p>
-        <a href="">leam more</a>
-      </div>
-      
-      <div class="card">
-        <img src="" alt="">
-        <div class="title">
-          <h3>裂縫之下</h3>
-          <p>Beneath the Fracture</p>
-          <p>油畫</p>
-        </div>
-        <p>本畫靈感來自她親眼目睹的冰川裂縫，畫面以大面積裂痕與流動線條組成，象徵人內在的情緒斷層。她用此畫探討壓抑情感之下的潛流，並相信裂縫不是崩壞，而是讓光進入的通道。</p>
-        <a href="">leam more</a>
-      </div>
-
-      <div class="card">
-        <img src="" alt="">
-        <div class="title">
-          <h3>裂縫之下</h3>
-          <p>Beneath the Fracture</p>
-          <p>油畫</p>
-        </div>
-        <p>本畫靈感來自她親眼目睹的冰川裂縫，畫面以大面積裂痕與流動線條組成，象徵人內在的情緒斷層。她用此畫探討壓抑情感之下的潛流，並相信裂縫不是崩壞，而是讓光進入的通道。</p>
-        <a href="">leam more</a>
-      </div>
-      
     </div>
 
 
 
 
   </div>
-  
+
 </template>
 
 <script setup>
