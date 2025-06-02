51dd9ce9ad8aa9ab8098615b015a599f4b85181c
ivyraine <ivy00910@gmail.com>
2025-06-02 15:17:24 +0800

    1. 產品分頁卡片點選後重新刷新分頁 2. 商城標籤顏色配合標題修改為黑色

--- src/router/index.js
+++ src/router/index.js
@@ -110,12 +110,14 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 }
+  },
 })
 
 export default router
-
-
 
 
 
